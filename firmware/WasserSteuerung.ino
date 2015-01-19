dac2433904e1f3cf2c768968a82e022f6eeb45d6
chkummer <chkummer@gmail.com>
2015-01-19 22:23:56 +0100

    Konsolenausgabe und Dokumentation
    Konsolenausgabe angepasst damit nun Zustandsaenderungen des Ventils ausgegeben wird. Konsolenausgabe der Werte vereinheitlicht. Dokumentation ins Wiki verlagert.

--- firmware/WasserSteuerung.ino
+++ firmware/WasserSteuerung.ino
@@ -2,7 +2,7 @@
  * 17-Jan-2015
  * Christoph Kummer
  * 
- * Wasser Steuerung: 
+ * WasserSteuerung / Version 1.1: 
  *      Diese Applikation soll mittels eines Sensors erkennen ob der Kanister bereist voll ist und 
  *      entsprechend das Magnetventil schliessen. Der Sensor besteht lediglich aus zwei Draehten  
  *      (eine Masse und der zweite am Analog/Digital-Wander angeschlossen). Das Magnetventil stammt  
@@ -53,7 +53,7 @@ void setup()
   //digitalWrite(2, HIGH); //Hack for getting around INPUT_PULLUP
 
   mySerial.begin(9600);
-  mySerial.println("WasserSteuerung"); // Say hello
+  mySerial.println("WasserSteuerung/V1.1"); // Say hello
   digitalWrite(waterValve, LOW); //Turn off valve
   
   //Take a series of readings from the water sensor and average them
@@ -68,6 +68,7 @@ void setup()
   mySerial.print("Avg: ");
   mySerial.println(waterAvg);
 
+  mySerial.println("ACTION: opening Valve");
   digitalWrite(waterValve, HIGH); //Turn on valve
 }
  
@@ -75,11 +76,14 @@ void loop()
 {
   int waterDifference = abs(analogRead(waterSensor) - waterAvg);
   mySerial.print("Diff: ");
-  mySerial.println(waterDifference);
+  mySerial.print(waterDifference);
+  mySerial.print(" / Read: ");
+  mySerial.println(analogRead(waterSensor));
 
   if(waterDifference > maxDifference) // Water detected turn off valve
   {
     long startTime = millis(); //Record the current time
+    mySerial.println("ACTION: closing Valve");
     digitalWrite(waterValve, LOW); //Turn off valve
 
     //Loop until we don't detect water AND 2 seconds of alarm have completed
@@ -87,11 +91,14 @@ void loop()
     {
       waterDifference = abs(analogRead(waterSensor) - waterAvg); //Take a new reading
 
-      mySerial.print("Read: ");
+      mySerial.print("Diff: ");
+      mySerial.print(waterDifference);
+      mySerial.print(" / Read: ");
       mySerial.println(analogRead(waterSensor));
       delay(1000);
     } //Loop until we don't detect water AND 2 seconds of alarm have completed
 
+    mySerial.println("ACTION: opening Valve");
     digitalWrite(waterValve, HIGH); //Turn on valve
   }
   delay(1000);
